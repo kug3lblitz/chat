79d8d8f9ec84ff92950c10a938467adc8e0503fc
Patrick Werner <pwerner2@gmail.com>
2016-10-11 23:46:42 -0400

    getting a weird box-in-box issue

--- index.php
+++ index.php
@@ -22,8 +22,16 @@ if(isset($_POST['enter'])){
         echo '<span class="error">Please type in a name</span>';
     }
 }
-?>
 
+if (isset($_GET['logout'])){
+    $fp = fopen("log.html", 'a');
+    fwrite ( $fp, "<div class='msgln'><i>User " . $_SESSION ['name'] . " has left the chat session.</i><br></div>" );
+    fclose ($fp);
+
+    session_destroy();
+    header("Location: index.php");
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -111,7 +119,17 @@ else{
             <div style="clear:both"></div>
         </div>
 
-        <div id="chatbox"></div>
+        <div id="chatbox">
+            <?php
+              if(file_exists("log.html") && filesize("log.html") > 0){
+                $handle = fopen("log.html", "r");
+                $contents = fread($handle, filesize("log.html"));
+                fclose($handle);
+
+                echo $contents;
+              }
+            ?>
+        </div>
 
         <form name="message" action="">
             <input name="usermsg" type="text" id="usermsg" size="63">
@@ -121,6 +139,45 @@ else{
     </div>
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+<script>
+
+$(document).ready(function(){
+  //If user wants to end session
+  $("#exit").click(function(){
+    var exit = confirm("Are you sure you want to end the session?");
+    if(exit==true){window.location = 'index.php?logout=true';}     
+  });
+});
+ 
+//If user submits the form
+$("#submitmsg").click(function(){
+  var clientmsg = $("#usermsg").val();
+  $.post("post.php", {text: clientmsg});             
+  $("#usermsg").prop("value", "");
+  loadLog;
+  return false;
+});
+ 
+function loadLog(){    
+  var oldscrollHeight = $("#chatbox").prop("scrollHeight") - 20; //Scroll height before the request
+  $.ajax({
+    url: "log.html",
+    cache: false,
+    success: function(html){       
+      $("#chatbox").html(html); //Insert chat log into the #chatbox div  
+           
+      //Auto-scroll          
+      var newscrollHeight = $("#chatbox").prop("scrollHeight") - 20; //Scroll height after the request
+      if(newscrollHeight > oldscrollHeight){
+        $("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
+        }              
+    },
+  });
+}
+ 
+setInterval (loadLog, 2500);
+
+</script>
 <?php
 }
 ?>
